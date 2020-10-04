f8eab0e427215f68b6b5a67a036a0dc009a385e8
alexander-kolmachikhin <alexander-kolmachikhin@mail.ru>
2020-10-04 17:40:12 +0700

    refactor

--- app/src/main/java/simple/cypher/presentation/MainActivity.kt
+++ app/src/main/java/simple/cypher/presentation/MainActivity.kt
@@ -1,6 +1,42 @@
 package simple.cypher.presentation
 
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import simple.cypher.R
 
-class MainActivity : AppCompatActivity(R.layout.main_activity)
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme()
+        setStatusBar()
+        setLayout()
+    }
+
+    fun setTheme() = setTheme(
+        when (isNightModeEnabled()) {
+            true -> R.style.DarkTheme
+            else -> R.style.LightTheme
+        }
+    )
+
+    fun setStatusBar() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (!isNightModeEnabled())
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+        }
+    }
+
+    fun setLayout() = setContentView(R.layout.main_activity)
+
+
+    fun isNightModeEnabled() =
+        resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+}

568da416aebb1edc4a58ac24ac1dc32b46516722
alexander-kolmachikhin <alexander-kolmachikhin@mail.ru>
2020-10-04 17:52:16 +0700

    refactor

--- app/src/main/java/simple/cypher/presentation/MainActivity.kt
+++ app/src/main/java/simple/cypher/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package simple.cypher.presentation
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import simple.cypher.R
@@ -29,12 +30,12 @@ class MainActivity : AppCompatActivity() {
             if (!isNightModeEnabled())
                 window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         } else {
-            window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+            //window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         }
     }
 
     fun setLayout() = setContentView(R.layout.main_activity)
-
 
     fun isNightModeEnabled() =
         resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
